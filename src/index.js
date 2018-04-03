daeb4a3d3cda6199d0ee339475868d7cb8d4cd8f
Ben <a@ben.am>
2018-04-03 14:40:05 +0100

    css (inlined in js bundle)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import '../styles/image_viewer.css';
+
 import {append_img} from './image_viewer';
 append_img();
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ const config = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
     ],
   },
 
